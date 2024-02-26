eaf6f3542bf3225927790f6bf107d74e01c262fc
martin265 <martinsilungwe12@gmail.com>
2024-02-26 13:10:30 +0200

    still in progress

---
+++ administrator_interview_questions.php
@@ -0,0 +1,25 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="main-content-container">
+        
+        <div class="side-navigation-area">
+            <?php include("administrator_sidebar.php"); ?>
+        </div>
+
+        <!-- ============ main content container will be here ============ -->
+        <div class="content-area">
+
+        </div>
+    </div>
+</body>
+</html>
