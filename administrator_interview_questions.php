313e7d8d54c26e1653f12de1ca3c19241a7b7504
martin265 <martinsilungwe12@gmail.com>
2024-02-26 13:26:52 +0200

    progress

--- administrator_interview_questions.php
+++ administrator_interview_questions.php
@@ -22,7 +22,30 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="interview-questions-panel">
-                            
+                            <div class="interview-questions-panel-title">
+                                <h1>interview questions</h1>
+                            </div>
+
+                            <!-- =============== main panel for the questions here ========= -->
+                            <div class="interview-questions-panel-form">
+                                <form action="administrator_interview_questions.php" method="POST">
+                                    <div class="row">
+                                        <div class="col">
+                                            <label for="ForQuestion1" class="fw-bold ps-2">Question 1</label>
+                                            <div class="input-group">
+                                                <input type="text" name="question_1" class="form-control form-control-lg">
+                                            </div>
+                                        </div>
+
+                                        <div class="col">
+                                            <label for="ForQuestion1" class="fw-bold ps-2">Question 1</label>
+                                            <div class="input-group">
+                                                <input type="text" name="question_1" class="form-control form-control-lg">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
