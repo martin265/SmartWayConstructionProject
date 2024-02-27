0450001d875f4c2d7a6b7b7fc4991b8059bbc63c
martin265 <martinsilungwe12@gmail.com>
2024-02-27 11:28:41 +0200

    still working on it

--- records.php
+++ records.php
@@ -10,6 +10,22 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="main-content-container">
+        <!-- ========== main content for the page will be here ====== -->
+        <div class="side-navigation-area">
+            <?php include("administrator_sidebar.php"); ?>
+        </div>
+
+        <!-- ============ the main content area will be here ======== -->
+        <div class="content-area">
+            <div class="container-xxl">
+                <div class="row">
+                    <div class="col-lg-12">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
