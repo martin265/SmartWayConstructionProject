11bed41b9e8e55b9f5c3e97f44b11e91213bb3dc
martin265 <martinsilungwe12@gmail.com>
2024-03-01 17:53:03 +0200

    working on the applicant application form

--- applicant_apply_job.php
+++ applicant_apply_job.php
@@ -12,5 +12,30 @@
     <div class="main-navigation-bar">
         <?php include("templates/header.php"); ?>
     </div>
+
+    <div class="container-xxl">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="applicant-apply-job-page">
+                    <div class="applicant-apply-job-page-form">
+                        <form action="applicant_apply_job.php" method="POST" enctype="">
+                            <div class="row">
+                                <div class="col">
+                                    <label for="FirstName" class="fw-bold">First Name</label>
+                                    <input type="text" name="first_name" class="form-control form-control-lg">
+                                </div>
+
+                                <!-- ============= for the last name here ========= -->
+                                <div class="col">
+                                    <label for="FirstName" class="fw-bold">First Name</label>
+                                    <input type="text" name="first_name" class="form-control form-control-lg">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
