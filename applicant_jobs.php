db5947b6241a3f19be6ff92f57283521a3c0ff9a
martin265 <martinsilungwe12@gmail.com>
2024-03-01 11:00:48 +0200

    dont over commit

--- applicant_jobs.php
+++ applicant_jobs.php
@@ -41,7 +41,7 @@
 
                             <div class="applicant-jobs-single-card-features">
                                 <div class="applicant-jobs-single-card-features-1 shadow-lg">
-                                    
+
                                     <div class="applicant-jobs-single-card-features-title">
                                         <i class="bi bi-intersect"></i>
                                     </div>
@@ -83,8 +83,12 @@
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
+
+    <!-- ============== the container for all the available jobs will be here ======= -->
+    
 </body>
 </html>
