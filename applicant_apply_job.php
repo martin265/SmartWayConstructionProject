317cc164eac417cd263eb2fa7a5a4f8270e22337
martin265 <martinsilungwe12@gmail.com>
2024-03-02 14:10:49 +0200

    working on the job applicant page

--- applicant_apply_job.php
+++ applicant_apply_job.php
@@ -27,50 +27,62 @@
 
                                 <!-- ============= for the last name here ========= -->
                                 <div class="col">
-                                    <label for="LastName" class="fw-bold">First Name</label>
-                                    <input type="text" name="first_name" class="form-control form-control-lg">
+                                    <label for="LastName" class="fw-bold">Last Name</label>
+                                    <input type="text" name="last_name" class="form-control form-control-lg">
                                 </div>
                             </div>
 
                             <div class="row mt-3">
                                 <div class="col">
-                                    <label for="FirstName" class="fw-bold">First Name</label>
-                                    <input type="text" name="first_name" class="form-control form-control-lg">
+                                    <label for="FirstName" class="fw-bold">Age</label>
+                                    <input type="text" name="age" class="form-control form-control-lg">
                                 </div>
 
                                 <!-- ============= for the last name here ========= -->
                                 <div class="col">
-                                    <label for="FirstName" class="fw-bold">First Name</label>
-                                    <input type="text" name="first_name" class="form-control form-control-lg">
-                                </div>
-                            </div>
-
-
-                            <div class="row mt-3">
-                                <div class="col">
-                                    <label for="FirstName" class="fw-bold">First Name</label>
-                                    <input type="text" name="first_name" class="form-control form-control-lg">
-                                </div>
-
-                                <!-- ============= for the last name here ========= -->
-                                <div class="col">
-                                    <label for="FirstName" class="fw-bold">First Name</label>
-                                    <input type="text" name="first_name" class="form-control form-control-lg">
+                                    <label for="FirstName" class="fw-bold">Gender</label>
+                                    <select name="gender" id="" class="form-control form-control-lg">
+                                        <option value="Male">Male</option>
+                                        <option value="Female">Female</option>
+                                    </select>
+                                    
                                 </div>
                             </div>
 
 
                             <div class="row mt-3">
                                 <div class="col">
-                                    <label for="FirstName" class="fw-bold">First Name</label>
-                                    <input type="text" name="first_name" class="form-control form-control-lg">
+                                    <label for="FirstName" class="fw-bold">Phone Number</label>
+                                    <input type="text" name="phone_number" class="form-control form-control-lg">
                                 </div>
 
                                 <!-- ============= for the last name here ========= -->
                                 <div class="col">
-                                    <label for="FirstName" class="fw-bold">First Name</label>
-                                    <input type="text" name="first_name" class="form-control form-control-lg">
+                                    <label for="FirstName" class="fw-bold">Email</label>
+                                    <input type="email" name="first_name" class="form-control form-control-lg">
                                 </div>
+                            </div>
+
+
+                            <div class="row mt-3">
+                                <div class="col">
+                                    <label for="FirstName" class="fw-bold">Marital Status</label>
+                                    <select name="marital_status" id="" class="form-control form-control-lg">
+                                        <option value="Married">Married</option>
+                                        <option value="Single">Single</option>
+                                        <option value="Divorced">Divorced</option>
+                                    </select>
+                                </div>
+
+                                <!-- ============= for the last name here ========= -->
+                                <div class="col">
+                                    <label for="FirstName" class="fw-bold">Home address</label>
+                                    <input type="text" name="address" class="form-control form-control-lg">
+                                </div>
+                            </div>
+
+                            <div class="education-documents">
+                                
                             </div>
                         </form>
                     </div>
