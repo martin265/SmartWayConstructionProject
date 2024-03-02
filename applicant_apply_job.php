2161889dbdcd2e41784f34e3e4b9638a21938abe
martin265 <martinsilungwe12@gmail.com>
2024-03-02 14:26:13 +0200

    working on the pages

--- applicant_apply_job.php
+++ applicant_apply_job.php
@@ -21,7 +21,7 @@
                         <form action="applicant_apply_job.php" method="POST" enctype="">
                             <div class="row">
                                 <div class="col">
-                                    <label for="FirstName" class="fw-bold">First Name</label>
+                                    <label for="FirstName" class="fw-bold ms-2">First Name</label>
                                     <div class="input-group">
                                         <span class="input-group-text"><i class="bi bi-body-text"></i></span>
                                         <input type="text" name="first_name" class="form-control form-control-lg">
@@ -30,7 +30,7 @@
 
                                 <!-- ============= for the last name here ========= -->
                                 <div class="col">
-                                    <label for="LastName" class="fw-bold">Last Name</label>
+                                    <label for="LastName" class="fw-bold ms-2">Last Name</label>
                                     <div class="input-group">
                                         <span class="input-group-text"><i class="bi bi-body-text"></i></span>
                                         <input type="text" name="last_name" class="form-control form-control-lg">
@@ -40,7 +40,7 @@
 
                             <div class="row mt-3">
                                 <div class="col">
-                                    <label for="FirstName" class="fw-bold">Age</label>
+                                    <label for="FirstName" class="fw-bold ms-2">Age</label>
                                     <div class="input-group">
                                         <span class="input-group-text"><i class="bi bi-123"></i></span>
                                         <input type="text" name="age" class="form-control form-control-lg">
@@ -49,7 +49,7 @@
 
                                 <!-- ============= for the last name here ========= -->
                                 <div class="col">
-                                    <label for="FirstName" class="fw-bold">Gender</label>
+                                    <label for="FirstName" class="fw-bold ms-2">Gender</label>
                                     <div class="input-group">
                                         <span class="input-group-text"><i class="bi bi-gender-ambiguous"></i></span>
                                         <select name="gender" id="" class="form-control form-control-lg">
@@ -63,7 +63,7 @@
 
                             <div class="row mt-3">
                                 <div class="col">
-                                    <label for="FirstName" class="fw-bold">Phone Number</label>
+                                    <label for="FirstName" class="fw-bold ms-2">Phone Number</label>
                                     <div class="input-group">
                                         <span class="input-group-text"><i class="bi bi-phone"></i></span>
                                         <input type="text" name="phone_number" class="form-control form-control-lg">
@@ -72,7 +72,7 @@
 
                                 <!-- ============= for the last name here ========= -->
                                 <div class="col">
-                                    <label for="FirstName" class="fw-bold">Email</label>
+                                    <label for="FirstName" class="fw-bold ms-2">Email</label>
                                     <div class="input-group">
                                         <span class="input-group-text"><i class="bi bi-envelope-fill"></i></span>
                                         <input type="email" name="first_name" class="form-control form-control-lg">
@@ -83,7 +83,7 @@
 
                             <div class="row mt-3">
                                 <div class="col">
-                                    <label for="FirstName" class="fw-bold">Marital Status</label>
+                                    <label for="FirstName" class="fw-bold ms-2">Marital Status</label>
                                     <div class="input-group">
                                         <span class="input-group-text"><i class="bi bi-person-hearts"></i></span>
                                         <select name="marital_status" id="" class="form-control form-control-lg">
@@ -96,7 +96,7 @@
 
                                 <!-- ============= for the last name here ========= -->
                                 <div class="col">
-                                    <label for="FirstName" class="fw-bold">Home address</label>
+                                    <label for="FirstName" class="fw-bold ms-2">Home address</label>
                                     <div class="input-group">
                                         <span class="input-group-text"><i class="bi bi-house-add"></i></span>
                                         <input type="text" name="address" class="form-control form-control-lg">
@@ -105,7 +105,7 @@
                             </div>
 
                             <div class="available-job-documents">
-                                
+
                             </div>
 
                             <div class="education-documents">
@@ -113,7 +113,7 @@
                                 <div class="row mt-3">
                                     <div class="col">
                                         <label for="ForFile">
-                                            <span class="fw-bold"><i class="bi bi-file-earmark-person me-2"></i>Select Cv File</span>
+                                            <span class="fw-bold ms-2"><i class="bi bi-file-earmark-person me-2"></i>Select Cv File</span>
                                         </label>
                                         <div class="input-group">
                                             <input type="file" class="form-control form-control-lg" name="cv">
@@ -122,7 +122,7 @@
                                     <!-- ============== // ================ // -->
                                     <div class="col">
                                         <label for="ForCoverLetter">
-                                            <span class="fw-bold"><i class="bi bi-postcard me-2"></i>Upload Cover Letter</span>
+                                            <span class="fw-bold ms-2"><i class="bi bi-postcard me-2"></i>Upload Cover Letter</span>
                                         </label>
                                         <div class="input-group">
                                             <input type="file" class="form-control form-control-lg" name="cover_letter">
