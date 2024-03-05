e94c0e784dcdccc9a51343010bce03c3cd64e777
martin265 <martinsilungwe12@gmail.com>
2024-03-05 17:55:34 +0200

    binding parameters for the prepared statements

--- Models/Applicant.php
+++ Models/Applicant.php
@@ -34,8 +34,20 @@ class Applicant{
     // ============ the function to save the records will be here ========= //
     public function saveApplicantDetails($conn) {
         try {
-
-
+            $sqlCommand = $conn->prepare(
+                "INSERT INTO ApplicantDetails (
+                    first_name, last_name, age, gender,
+                    phone_number, email, marital_status,
+                    home_address, cv, cover_letter
+                ) VALUES (?,?,?,?,?,?,?,?,?,?)"
+            );
+            // ======= binding the parameters here ========= //
+            $sqlCommand->bind_param(
+                "ssssssssss",
+                $this->first_name, $this->last_name, $this->age,
+                $this->gender, $this->phone_number, $this->email,
+                $this->marital_status, $this->home_address, $this->cv, $this->cover_letter
+            )
         }catch(Exception $ex){
             print($ex);
         }
