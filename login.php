3577c7d272db7d34eec06ce7991295852ec45797
martin265 <martinsilungwe12@gmail.com>
2024-03-10 16:58:15 +0200

    css for the login page

--- login.php
+++ login.php
@@ -43,7 +43,7 @@
             <div class="col-lg-12">
                 <div class="main-login-page">
                     <div class="center-login-panel">
-
+                        
                     </div>
                 </div>
             </div>
