a776acd2124c2f881bb887275db3da28c8bfd277
martin265 <martinsilungwe12@gmail.com>
2024-03-13 10:21:30 +0200

    first commit of the day

--- administrator_application.php
+++ administrator_application.php
@@ -44,9 +44,43 @@ $all_results = fetchAllApplicants($conn);
                             </div>
 
                             <div class="all-available-applications">
-                                <?php foreach($all_results as $single_record){?>
-                                    
-                                <?php }?>
+                                <table id="recent-table" class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col" class="text-capitalize">job title</th>
+                                            <th scope="col" class="text-capitalize">job type</th>
+                                            <th scope="col" class="text-capitalize">job email</th>
+                                            <th scope="col" class="text-capitalize">job phone number</th>
+                                            <th scope="col" class="text-capitalize">qualification</th>
+                                            <th scope="col" class="text-capitalize">technical skills</th>
+                                            <th scope="col" class="text-capitalize">actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <!-- ============== looping through the results here ====== -->
+                                        <?php if ($all_results): ?>
+                                            <?php foreach($all_results as $single_record) {?>
+                                                <tr>
+                                                    <td><?php echo($single_record["job_title"]); ?></td>
+                                                    <td><?php echo($single_record["job_type"]); ?></td>
+                                                    <td><?php echo($single_record["job_email"]); ?></td>
+                                                    <td><?php echo($single_record["job_phone_number"]); ?></td>
+                                                    <td><?php echo($single_record["qualification"]); ?></td>
+                                                    <td><?php echo($single_record["technical_skills"]); ?></td>
+                                                    <!-- ============ for the button here -->
+                                                    <td>
+                                                        <form action="administrator_job_records.php" method="POST">
+                                                            <input type="hidden" name="id_to_delete" value="<?php echo($single_record["job_id"]); ?>">
+                                                            <input type="submit" name="delete_record" value="Delete Record" class="btn btn-sm btn-danger">
+                                                        </form>
+                                                    </td>
+                                                </tr>
+                                            <?php }?>
+                                        <?php else: ?>
+
+                                        <?php endif; ?>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
