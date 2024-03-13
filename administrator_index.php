bb712edec962ec8b61682dd27af1bf48cbb71c77
martin265 <martinsilungwe12@gmail.com>
2024-03-13 12:36:06 +0200

    updating the controls on the dashboard

--- administrator_index.php
+++ administrator_index.php
@@ -20,8 +20,12 @@
             <div class="container-xxl">
                 <div class="row">
                     <div class="col-lg-12">
-                        <div class="main-dashboard">
-                            
+                        <div class="main-dashboard shadow-sm">
+                            <div class="main-dashboard-cards">
+                                <div class="main-dashboard-questions">1</div>
+                                <div class="main-dashboard-jobs">2</div>
+                                <div class="main-dashboard-applicants">3</div>
+                            </div>
                         </div>
                     </div>
                 </div>
