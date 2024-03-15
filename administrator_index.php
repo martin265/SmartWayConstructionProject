fe61e492653321d5d07948c7ba96507622cf3729
martin265 <martinsilungwe12@gmail.com>
2024-03-15 17:39:51 +0200

    working on the report part of the project

--- administrator_index.php
+++ administrator_index.php
@@ -22,6 +22,23 @@ function countQuestionRecords($conn) {
 }
 $totalQuestions = countQuestionRecords($conn);
 
+// function to fetch the records here ======= //
+function fetchAllApplicants($conn) {
+    try {
+        $sqlCommand = "SELECT * FROM ApplicantDetails";
+        // ======== getting the results here ======== //
+        $results = mysqli_query($conn, $sqlCommand);
+        // ======= converting the results into an array here ========= //
+        $all_results = mysqli_fetch_all($results, MYSQLI_ASSOC);
+        
+        return $all_results;
+
+    }catch(Exception $ex) {
+        print($ex);
+    }
+}
+
+$all_results = fetchAllApplicants($conn);
 // ============= function to fetching all the available jobs here ========= //
 function countJobRecords($conn) {
     try {
@@ -123,8 +140,51 @@ $totalJobs = countJobRecords($conn);
                                     <h1>recent activity</h1>
                                 </div>
 
-                                <div class="recent-activity-table">
-                                    
+                                
+                            </div>
+
+                            <div class="all-available-applications">
+                                <div class="all-available-applicants-table">
+                                    <table id="applicant-details" class="table table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col" class="text-capitalize">first name</th>
+                                                <th scope="col" class="text-capitalize">last name</th>
+                                                <th scope="col" class="text-capitalize">age</th>
+                                                <th scope="col" class="text-capitalize">gender</th>
+                                                <th scope="col" class="text-capitalize">phone_number</th>
+                                                <th scope="col" class="text-capitalize">home address</th>
+                                                <th scope="col" class="text-capitalize">job title</th>
+                                                <th scope="col" class="text-capitalize">operations</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <!-- ============== looping through the results here ====== -->
+                                            <?php if ($all_results): ?>
+                                                <?php foreach($all_results as $single_record) {?>
+                                                    <tr>
+                                                        <td><?php echo($single_record["first_name"]); ?></td>
+                                                        <td><?php echo($single_record["last_name"]); ?></td>
+                                                        <td><?php echo($single_record["age"]); ?></td>
+                                                        <td><?php echo($single_record["gender"]); ?></td>
+                                                        <td><?php echo($single_record["phone_number"]); ?></td>
+                                                        <td><?php echo($single_record["home_address"]); ?></td>
+                                                        <td><?php echo($single_record["job_title"]); ?></td>
+                                                        <!-- ============ for the button here -->
+                                                        <td>
+                                                            <form action="administrator_application.php" method="POST">
+                                                                <input type="hidden" name="id_to_delete" value="<?php echo($single_record["applicant_id"]); ?>">
+                                                                <input type="submit" name="delete_record" value="Delete Record" class="btn btn-sm btn-danger">
+                                                            </form>
+                                                        </td>
+                                                    </tr>
+
+                                                <?php }?>
+                                            <?php else: ?>
+
+                                            <?php endif; ?>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </div>
