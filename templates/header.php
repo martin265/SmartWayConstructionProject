64cbab1efbd549614a9a7014ab2896a8787c33bc
martin265 <martinsilungwe12@gmail.com>
2024-03-16 09:30:37 +0200

    fixing the header for the website here

--- templates/header.php
+++ templates/header.php
@@ -48,25 +48,30 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     <div class="top-navigation-bar">
         <nav class="navbar navbar-expand-lg bg-primary h-50">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar w/ text</a>
+                <a class="navbar-brand" href="#">
+                    <img src="assets\landing\house.png" alt="">
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                </ul>
-                <span class="navbar-text">
-                    Navbar text with an inline element
-                </span>
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <!-- ================= // ================= // -->
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <!-- ================ the other navigation element will be here =====  -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <!-- =================== // ================== // -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                    </ul>
+                    <span class="navbar-text text-bold">
+                        SmartWay Construction
+                    </span>
                 </div>
             </div>
         </nav>
