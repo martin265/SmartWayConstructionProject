53160dbb20d2e2436a6d0e157948d29c24ada932
martin265 <martinsilungwe12@gmail.com>
2024-03-18 13:26:24 +0200

    chaning the tables

--- administrator_report.php
+++ administrator_report.php
@@ -42,8 +42,42 @@ $all_results = fetchAllApplicants($conn);
                                 <h1>send progress and notifications</h1>
                             </div>
                             <!-- ============ the table for the applicants here ======= // -->
-                            <div class="applicants-table">
+                            <div class="all-available-applications">
+                                <div class="all-available-applicants-table">
+                                    <table id="applicant-details" class="table table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col" class="text-capitalize">first name</th>
+                                                <th scope="col" class="text-capitalize">last name</th>
+                                                <th scope="col" class="text-capitalize">age</th>
+                                                <th scope="col" class="text-capitalize">gender</th>
+                                                <th scope="col" class="text-capitalize">phone_number</th>
+                                                <th scope="col" class="text-capitalize">home address</th>
+                                                <th scope="col" class="text-capitalize">job title</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <!-- ============== looping through the results here ====== -->
+                                            <?php if ($all_results): ?>
+                                                <?php foreach($all_results as $single_record) {?>
+                                                    <tr>
+                                                        <td><?php echo($single_record["first_name"]); ?></td>
+                                                        <td><?php echo($single_record["last_name"]); ?></td>
+                                                        <td><?php echo($single_record["age"]); ?></td>
+                                                        <td><?php echo($single_record["gender"]); ?></td>
+                                                        <td><?php echo($single_record["phone_number"]); ?></td>
+                                                        <td><?php echo($single_record["home_address"]); ?></td>
+                                                        <td><?php echo($single_record["job_title"]); ?></td>
+                                                        <!-- ============ for the button here -->
+                                                    </tr>
 
+                                                <?php }?>
+                                            <?php else: ?>
+
+                                            <?php endif; ?>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
 
                         </div>
